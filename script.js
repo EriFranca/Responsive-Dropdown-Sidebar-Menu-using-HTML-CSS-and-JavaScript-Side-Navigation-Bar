5e68087f6de85d7e5477a932a10aa357f5688164
Eri França <87365333+EriFranca@users.noreply.github.com>
2021-11-27 13:00:27 -0300

    function myMove() {

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("close");
 });
+function myMove() {
+  let id = null;
+  const elem = document.getElementById("animate");   
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+    }
+  }
+}
