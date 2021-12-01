88b1b9105c04b556c284685c9b11dde9997875dc
Eri França <87365333+EriFranca@users.noreply.github.com>
2021-12-01 07:34:52 -0300

    TESTE

--- Plugins/js/main.js
+++ Plugins/js/main.js
@@ -43,17 +43,4 @@ BOOK_TABLE.on('click', '.book-edit', handleBookEdit);
 BOOK_TABLE.on('click', '.book-delete', handleBookDeleteClick);
 BOOK_FORM.on('submit', handleBookSubmission);
 
-let arrow = document.querySelectorAll(".arrow");
-for (var i = 0; i < arrow.length; i++) {
-  arrow[i].addEventListener("click", (e)=>{
- let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
- arrowParent.classList.toggle("showMenu");
-  });
-}
 
-let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".bx-menu");
-console.log(sidebarBtn);
-sidebarBtn.addEventListener("click", ()=>{
-  sidebar.classList.toggle("close");
-});
