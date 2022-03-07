d779c6a4b44b185c83741d2b98543d97bf4a5ac8
Eri França <87365333+EriFranca@users.noreply.github.com>
2022-03-07 12:37:31 -0300

    title: data(getHour),

--- js/book-form.js
+++ js/book-form.js
@@ -7,20 +7,20 @@ const bookForm = (function($){
     const TIME =$("#hora");
     const BOOK_UPDATE_BUTTON = $("#updateButton");
 
-    function pegaData() {
-        var data = new Date();
-    
-        var hora    = data.getHours();          // 0-23
-        var min     = data.getMinutes();        // 0-59
-        var seg     = data.getSeconds();        // 0-59
-        var mseg    = data.getMilliseconds();   // 0-999
-        var tz      = data.getTimezoneOffset(); // em minutos
-    
-        // Formata a data e a hora (note o mês + 1)
-        var str_hora = hora + ':' + min + ':' + seg;
-        document.getElementById("recebeHora").innerHTML = "Hora da Entrada"+str_hora;
-        }
-        
+    // Obtém a data/hora atual
+var data = new Date();
+
+// Guarda cada pedaço em uma variável
+var dia     = data.getDate();           // 1-31
+var dia_sem = data.getDay();            // 0-6 (zero=domingo)
+var mes     = data.getMonth();          // 0-11 (zero=janeiro)
+var ano2    = data.getYear();           // 2 dígitos
+var ano4    = data.getFullYear();       // 4 dígitos
+var hora    = data.getHours();          // 0-23
+var min     = data.getMinutes();        // 0-59
+var seg     = data.getSeconds();        // 0-59
+var mseg    = data.getMilliseconds();   // 0-999
+var tz      = data.getTimezoneOffset(); // em minutos
     
     function clear() {
         setData();
@@ -34,6 +34,7 @@ const bookForm = (function($){
     function getData() {
         return {
             title: BOOK_TITLE.val(),
+            title: data(getHour),
             author: BOOK_AUTHOR.val(),
             publisher: BOOK_PUBLISHER.val(),
             agrupamento: AGRUP.val(),
