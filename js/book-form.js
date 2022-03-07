6e9b93a3eda2ef19ed59182b821c9ed09ff50123
Eri França <87365333+EriFranca@users.noreply.github.com>
2022-03-07 11:55:54 -0300

    return BOOK_UPDATE_BUTTON.text();         return pegaData;

--- js/book-form.js
+++ js/book-form.js
@@ -7,6 +7,20 @@ const bookForm = (function($){
     const TIME =$("#hora");
     const BOOK_UPDATE_BUTTON = $("#updateButton");
 
+    function pegaData() {
+        var data = new Date();
+    
+        var hora    = data.getHours();          // 0-23
+        var min     = data.getMinutes();        // 0-59
+        var seg     = data.getSeconds();        // 0-59
+        var mseg    = data.getMilliseconds();   // 0-999
+        var tz      = data.getTimezoneOffset(); // em minutos
+    
+        // Formata a data e a hora (note o mês + 1)
+        var str_hora = hora + ':' + min + ':' + seg;
+        document.getElementById("recebeHora").innerHTML = "Hora da Entrada"+str_hora;
+        }
+        
     
     function clear() {
         setData();
@@ -44,6 +58,7 @@ const bookForm = (function($){
 
     function getSubmitButtonText() {
         return BOOK_UPDATE_BUTTON.text();
+        return pegaData;
     }
 
     return {
